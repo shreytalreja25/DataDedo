b5a5efb1777701cce7b5ef3755a89f092d737ad7
Shrey Talreja <shreytalreja25@gmail.com>
2023-08-06 18:35:05 +0530

    Changed Connection String

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/DataDedo', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      'mongodb+srv://shreytalreja25:Shrey9999@cluster0.rpctqkh.mongodb.net/test?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
